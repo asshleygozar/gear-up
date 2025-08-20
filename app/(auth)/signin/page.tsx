4ffc4701d3b434c51d1d708a48f24ca6bb7f4af0
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-20 22:10:06 +0800

    feat(design): implement ui design using shadcn component and tailwind classes

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -3,14 +3,15 @@
 import { useActionState } from 'react';
 import { useRouter } from 'next/navigation';
 import {
-	Button,
 	Form,
 	FormError,
 	FormGroup,
 	FormInput,
 	FormLabel,
 } from '@/components/ui/Form';
+import { Button } from '@/components/ui/button';
 import { signIn, ActionResponse } from '@/lib/auth';
+import { cn } from '@/lib/utils';
 
 const initialState: ActionResponse = {
 	success: false,
@@ -52,18 +53,14 @@ export default function SignInPage() {
 	>(handleSubmit, initialState);
 
 	return (
-		<div className='h-screen bg-blue-200 flex justify-center items-center'>
+		<div className='h-screen flex justify-center items-center'>
 			<Form
 				action={formAction}
-				className='bg-white rounded-xl p-[2rem] w-md h-fit flex flex-col gap-[1rem] shadow-md'
+				className='bg-white rounded-xl p-[2rem] w-md h-fit flex flex-col gap-[1rem] shadow-lg'
 			>
 				<h1 className='text-center text-2xl'>Sign In</h1>
-				{state?.error && state?.message && (
-					<FormError className='text-red-300'>
-						{state.error} {state.message}
-					</FormError>
-				)}
-				<FormGroup>
+				<FormError className='text-red-400'>{state.message}</FormError>
+				<FormGroup className='flex flex-col gap-2'>
 					<FormLabel htmlFor='email'>Email</FormLabel>
 					<FormInput
 						id='email'
@@ -71,10 +68,14 @@ export default function SignInPage() {
 						type='email'
 						required
 						placeholder='Email'
-						className='border border-black-300 w-full bg-grey-300 rounded-md'
+						className={cn(
+							'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+							'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+							'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
+						)}
 					/>
 				</FormGroup>
-				<FormGroup>
+				<FormGroup className='flex flex-col gap-2'>
 					<FormLabel htmlFor='password'>Password</FormLabel>
 					<FormInput
 						id='password'
@@ -82,13 +83,17 @@ export default function SignInPage() {
 						type='password'
 						required
 						placeholder='Password'
-						className='border border-black-300 w-full bg-grey-300 rounded-md'
+						className={cn(
+							'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+							'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+							'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
+						)}
 					/>
 				</FormGroup>
 				<Button
 					type='submit'
-					isLoading={isPending}
-					className='bg-green-300 rounded-xl text-white p-[1rem]'
+					disabled={isPending}
+					className='bg-blue-400 text-white hover:bg-blue-300'
 				>
 					Sign in
 				</Button>
