c9042beddb8df77fed225847627d79b3eb14540b
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-21 14:56:44 +0800

    feat(marketing): add landing page ui design - add new color palette - implement sign in and sign up button for authentication - add brand name and tagline - implement navigation links for different marketing pages

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,78 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className='flex flex-col gap-15 justify-between'>
+			<nav className='flex justify-between items-center gap-10 h-[70px] py-[1rem] px-[3.5rem] shadow-md'>
+				<h1 className='text-header text-2xl font-quick-sand font-bold cursor-pointer self-start'>
+					Gear<span className='text-brand-foreground'>Up</span>
+				</h1>
+				<ul className='flex gap-5'>
+					<Link href={'/features'}>
+						<li className='text-sub-head font-inter font-semibold cursor-pointer'>
+							Features
+						</li>
+					</Link>
+					<Link href={'/about'}>
+						<li className='text-sub-head font-inter font-semibold cursor-pointer'>
+							About
+						</li>
+					</Link>
+					<Link href={'/pricing'}>
+						<li className='text-sub-head font-inter font-semibold cursor-pointer'>
+							Pricing
+						</li>
+					</Link>
+				</ul>
+				<div className='flex gap-5 self-end'>
+					<Link href={'/signin'}>
+						<Button className='font-quick-sand font-medium cursor-pointer'>
+							Sign in
+						</Button>
+					</Link>
+					<Link href={'/signup'}>
+						<Button
+							variant={'ghost'}
+							className='font-quick-sand font-medium cursor-pointer'
+						>
+							Sign up
+						</Button>
+					</Link>
+				</div>
+			</nav>
+			<main className='px-[3.5rem]'>
+				<section className='grid grid-cols-2 items-center h-[60vh]'>
+					<section className='flex flex-col gap-4'>
+						<h1 className='font-inter font-bold text-6xl'>
+							Take Control of Your{' '}
+							<span className='text-brand-foreground'>Finances</span>
+						</h1>
+						<h2 className='font-inter text-xl'>
+							Track expenses, manage savings, and achieve financial freedom with
+							ease.
+						</h2>
+						<div className='flex gap-5'>
+							<Link href={'/'}>
+								<Button className='bg-income font-inter text-lg px-[2rem] py-[1.5rem]'>
+									Get started
+								</Button>
+							</Link>
+							<Link href={'/'}>
+								<Button
+									variant={'outline'}
+									className='font-inter text-lg px-[2rem] py-[1.5rem]'
+								>
+									See demo
+								</Button>
+							</Link>
+						</div>
+					</section>
+					<section>Image here</section>
+				</section>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
