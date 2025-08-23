ef9c44464afaf020143518dc22e161e2ace7930b
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-23 22:03:53 +0800

    feat(session): implement shot term session for testing - implement session token verification for keeping the dashboard from being accessed by unauthorized users - refactor dashboard layout function to asychrounous

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,12 @@
 import LeftSidebar from '@/components/LeftSidebar';
+import { getCurrentUser } from '@/db/dal';
+import { redirect } from 'next/navigation';
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+	const getUser = await getCurrentUser();
+	if (!getUser) {
+		redirect('/signin');
+	}
 	return (
 		<main className='grid grid-cols-[250px_1fr]'>
 			<LeftSidebar />
