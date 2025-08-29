d6f10be892b39b9c5c0a42b8f6af51b54783d372
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-29 21:59:56 +0800

    refactor: update color palette and foregrounds - remove home module css for tailwind migration - implement brand component file for brand components - update primary and side bar background and foreground colors

---
+++ components/Brand.tsx
@@ -0,0 +1,7 @@
+export function BrandNameLogo() {
+	return (
+		<h1 className='text-primary-foreground text-3xl font-quick-sand font-semibold antialiased'>
+			Gear<span className='text-brand-foreground'>Up</span>
+		</h1>
+	);
+}
