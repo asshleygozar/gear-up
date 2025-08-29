e963979f7e3ed81d11cfc04671d0387ff4cde99a
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-29 21:48:42 +0800

    refactor: update left side bar components and contents - add responsive side bar from shadcn - add padding to sidebar ui component for mobile view to prevent padding collapse - remove all home page content for refactoring

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,66 +1,63 @@
-'use client';
-import styles from '@/styles/components/left-side-bar.module.css';
-import { NavigationButton } from './ui/Navbutton';
-import { useStorage } from '@/context/useStorage';
-import { usePathname } from 'next/navigation';
-import { Home, Account, Investment, Settings } from './ui/Icons';
+import { Home, CreditCard, Settings, Coins } from 'lucide-react';
 
-function LeftSidebar() {
-	const pathname = usePathname();
-	const { handleClick, setClick } = useStorage();
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import Link from 'next/link';
+import { BrandNameLogo } from './Brand';
+
+// Menu items.
+const items = [
+	{
+		title: 'Home',
+		url: '/dashboard/',
+		icon: Home,
+	},
+	{
+		title: 'Accounts',
+		url: '/dashboard/accounts',
+		icon: CreditCard,
+	},
+	{
+		title: 'Invesments',
+		url: '/dashboard/investments',
+		icon: Coins,
+	},
+	{
+		title: 'Settings',
+		url: '/dashboard/settings',
+		icon: Settings,
+	},
+];
+
+export function LeftSidebar() {
 	return (
-		<aside className={styles.container}>
-			<h1>Finance</h1>
-
-			<button
-				className={styles.newTransactionButton}
-				onClick={() => setClick(handleClick)}
-			>
-				Plus
-			</button>
-
-			<NavigationButton
-				className={
-					pathname === '/dashboard'
-						? `${styles.active} ${styles.navigationButton}`
-						: `${styles.navigationButton}`
-				}
-				icon={<Home />}
-				linkName='Home'
-				path='/dashboard/'
-			/>
-			<NavigationButton
-				className={
-					pathname === '/dashboard/accounts'
-						? `${styles.active} ${styles.navigationButton}`
-						: `${styles.navigationButton}`
-				}
-				icon={<Account />}
-				linkName='Accounts'
-				path='/dashboard/accounts'
-			/>
-			<NavigationButton
-				className={
-					pathname === '/dashboard/investments'
-						? `${styles.active} ${styles.navigationButton}`
-						: `${styles.navigationButton}`
-				}
-				icon={<Investment />}
-				linkName='Investments'
-				path='/dashboard/investments'
-			/>
-			<NavigationButton
-				className={
-					pathname === '/dashboard/settings'
-						? `${styles.active} ${styles.navigationButton}`
-						: `${styles.navigationButton}`
-				}
-				icon={<Settings />}
-				linkName='Settings'
-				path='/dashboard/settings'
-			/>
-		</aside>
+		<Sidebar className='p-[1rem]'>
+			<SidebarContent>
+				<BrandNameLogo />
+				<SidebarGroup>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<Link href={item.url}>
+											<item.icon />
+											<span>{item.title}</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
 	);
 }
-
-export default LeftSidebar;
