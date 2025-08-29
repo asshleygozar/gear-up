ce2e2546e6d8d9d87668e9a64c43a5b1d70ff47c
Asshley <asshleydeanwaynegozar@gmail.com>
2025-08-29 20:28:00 +0800

    refactor: update background and font color for theme branding

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,36 +5,36 @@ const Home = () => {
 	return (
 		<div className='flex flex-col gap-15 justify-between'>
 			<nav className='flex justify-between items-center gap-10 h-[70px] py-[1rem] px-[3.5rem] shadow-md'>
-				<h1 className='text-header text-2xl font-quick-sand font-bold cursor-pointer self-start'>
+				<h1 className='text-primary-foreground text-header text-2xl font-quick-sand font-bold cursor-pointer self-start'>
 					Gear<span className='text-brand-foreground'>Up</span>
 				</h1>
 				<ul className='flex gap-5'>
 					<Link href={'/features'}>
-						<li className='text-sub-head font-inter font-semibold cursor-pointer'>
+						<li className='text-primary-foreground font-inter font-semibold cursor-pointer'>
 							Features
 						</li>
 					</Link>
 					<Link href={'/about'}>
-						<li className='text-sub-head font-inter font-semibold cursor-pointer'>
+						<li className='text-primary-foreground font-inter font-semibold cursor-pointer'>
 							About
 						</li>
 					</Link>
 					<Link href={'/pricing'}>
-						<li className='text-sub-head font-inter font-semibold cursor-pointer'>
+						<li className='text-primary-foreground font-inter font-semibold cursor-pointer'>
 							Pricing
 						</li>
 					</Link>
 				</ul>
 				<div className='flex gap-5 self-end'>
 					<Link href={'/signin'}>
-						<Button className='font-quick-sand font-medium cursor-pointer'>
+						<Button className='bg-card text-brand-foreground font-quick-sand font-medium cursor-pointer'>
 							Sign in
 						</Button>
 					</Link>
 					<Link href={'/signup'}>
 						<Button
 							variant={'ghost'}
-							className='font-quick-sand font-medium cursor-pointer'
+							className='text-primary-foreground font-quick-sand font-medium cursor-pointer'
 						>
 							Sign up
 						</Button>
@@ -44,11 +44,11 @@ const Home = () => {
 			<main className='px-[3.5rem]'>
 				<section className='grid grid-cols-2 items-center h-[60vh]'>
 					<section className='flex flex-col gap-4'>
-						<h1 className='font-inter font-bold text-6xl'>
+						<h1 className='text-primary-foreground font-inter font-bold text-6xl'>
 							Take Control of Your{' '}
 							<span className='text-brand-foreground'>Finances</span>
 						</h1>
-						<h2 className='font-inter text-xl'>
+						<h2 className='text-primary-foreground font-inter text-xl'>
 							Track expenses, manage savings, and achieve financial freedom with
 							ease.
 						</h2>
@@ -61,7 +61,7 @@ const Home = () => {
 							<Link href={'/'}>
 								<Button
 									variant={'outline'}
-									className='font-inter text-lg px-[2rem] py-[1.5rem]'
+									className='text-primary-foreground font-inter text-lg px-[2rem] py-[1.5rem]'
 								>
 									See demo
 								</Button>
