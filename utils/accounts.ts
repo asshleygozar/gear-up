c8d72549134293d019abdda5b661411f3f11ca13
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-01 14:40:14 +0800

    feat(style): add responsive layout for tablet and mobile layout - remove unused imports - add dashboard layout full width and height - update util account for mocking user data - add absolute and relative position to sidebars to exclude sidebar button trigger from taking horizontal space

--- utils/accounts.ts
+++ utils/accounts.ts
@@ -1,98 +1,29 @@
-const accounts = [
+const user = [
 	{
 		id: 1,
-		balance: 2334,
-		accountName: 'GOTyme',
-		accountCategory: 'Bank',
-	},
-	{
-		id: 2,
-		balance: 1928,
-		accountName: 'Cash',
-		accountCategory: 'Cash',
-	},
-	{
-		id: 3,
-		balance: 2928,
-		accountName: 'SeaBank',
-		accountCategory: 'Bank',
-	},
-	{
-		id: 4,
-		balance: 2837,
-		accountName: 'BPI',
-		accountCategory: 'Bank',
-	},
-	{
-		id: 5,
-		balance: 9399,
-		accountName: 'BDO',
-		accountCategory: 'Bank',
-	},
-	{
-		id: 6,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-
-	{
-		id: 7,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-
-	{
-		id: 8,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-
-	{
-		id: 9,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-
-	{
-		id: 10,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-	{
-		id: 11,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-	{
-		id: 12,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-	{
-		id: 13,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-	{
-		id: 14,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
-	},
-	{
-		id: 15,
-		balance: 68362,
-		accountName: 'Maya',
-		accountCategory: 'E-Wallet',
+		email: 'asshleygozar@gmail.com',
+		name: 'Asshley',
+		accounts: [
+			{
+				cash: {
+					totalBalance: 2937,
+					totalIncome: 2800,
+					totalExpense: 137,
+				},
+			},
+		],
+		password: 'asshleygozar',
+		transactions: [
+			{
+				id: 1,
+				category: 'Food and drinks',
+				amount: 100,
+				account: 'Cash',
+				date: '08/01/2025',
+				description: '',
+			},
+		],
 	},
 ];
 
-export default accounts;
+export default user;
