27bb480ce5e85e05daf6d9a7c0963f2b17f09583
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-09 22:08:51 +0800

    update font color

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,7 +1,7 @@
 export function BrandNameLogo() {
 	return (
-		<h1 className='text-primary-foreground text-3xl font-quick-sand font-semibold antialiased'>
-			Gear<span className='text-brand'>Up</span>
+		<h1 className='text-black text-3xl font-quick-sand font-semibold antialiased'>
+			Gear<span className='text-black'>Up</span>
 		</h1>
 	);
 }
