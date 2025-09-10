34772de34a16100138db3d73c94917b85b7aac8c
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-10 10:09:38 +0800

    refactor: accounts page

--- app/dashboard/accounts/page.tsx
+++ app/dashboard/accounts/page.tsx
@@ -1,25 +1,10 @@
 'use client';
-import { useStorage } from '@/context/useStorage';
-import Modal from '@/components/NewTransactionModal';
-import styles from '@/styles/accounts.module.css';
-import { format } from 'date-fns';
-import accounts from '@/utils/accounts';
-import { useState, useEffect } from 'react';
 
 function Accounts() {
-	const { isClick } = useStorage();
-	const [timeStamp, setTimeStamp] = useState('');
-
-	useEffect(() => {
-		const formattedDate = format(new Date(), 'MMMM, dd, yyyy');
-		setTimeStamp(formattedDate);
-	}, []);
 	return (
-		<div className={styles.container}>
-			<section>{/* Total amount here*/}</section>
-			<section className={styles.accountsContainer}></section>
-			{isClick ? <Modal /> : ''}
-		</div>
+		<main>
+			<h1>Account</h1>
+		</main>
 	);
 }
 
