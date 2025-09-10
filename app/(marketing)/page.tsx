9ce7a542a92c0ba16c6d845dd42ac258d82050f1
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-10 12:24:57 +0800

    refactor(theme): update color palette and ui theme - update brand foreground - update hover background color - update global color palette using slate theme - fix non-existent foreground color - remove non-existent css global variables - update custom brand logo color

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import { BrandNameLogo } from '@/components/Brand';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
@@ -5,29 +6,30 @@ const Home = () => {
 	return (
 		<div className='flex flex-col gap-15 justify-between'>
 			<nav className='flex justify-between items-center gap-10 h-[70px] py-[1rem] px-[3.5rem] shadow-md'>
-				<h1 className='text-primary-foreground text-header text-2xl font-quick-sand font-bold cursor-pointer self-start'>
-					Gear<span className='text-brand'>Up</span>
-				</h1>
+				<BrandNameLogo />
 				<ul className='flex gap-5'>
 					<Link href={'/features'}>
-						<li className='text-primary-foreground font-inter font-semibold cursor-pointer'>
+						<li className='text-foreground font-inter font-semibold cursor-pointer'>
 							Features
 						</li>
 					</Link>
 					<Link href={'/about'}>
-						<li className='text-primary-foreground font-inter font-semibold cursor-pointer'>
+						<li className='text-foreground font-inter font-semibold cursor-pointer'>
 							About
 						</li>
 					</Link>
 					<Link href={'/pricing'}>
-						<li className='text-primary-foreground font-inter font-semibold cursor-pointer'>
+						<li className='text-foreground font-inter font-semibold cursor-pointer'>
 							Pricing
 						</li>
 					</Link>
 				</ul>
 				<div className='flex gap-5 self-end'>
 					<Link href={'/signin'}>
-						<Button className='bg-card font-quick-sand font-medium cursor-pointer'>
+						<Button
+							variant={'outline'}
+							className='bg-card text-card-foreground font-quick-sand font-medium cursor-pointer'
+						>
 							Sign in
 						</Button>
 					</Link>
@@ -44,23 +46,26 @@ const Home = () => {
 			<main className='px-[3.5rem]'>
 				<section className='grid grid-cols-2 items-center h-[60vh]'>
 					<section className='flex flex-col gap-4'>
-						<h1 className='text-primary-foreground font-inter font-bold text-6xl'>
+						<h1 className='text-foreground font-inter font-bold text-6xl'>
 							Take Control of Your <span className='text-brand'>Finances</span>
 						</h1>
-						<h2 className='text-primary-foreground font-inter text-xl'>
+						<h2 className='text-foreground font-inter text-xl'>
 							Track expenses, manage savings, and achieve financial freedom with
 							ease.
 						</h2>
 						<div className='flex gap-5'>
 							<Link href={'/'}>
-								<Button className='bg-income font-inter text-lg px-[2rem] py-[1.5rem] hover:text-brand'>
+								<Button
+									variant={'outline'}
+									className='bg-income font-inter text-lg px-[2rem] py-[1.5rem] hover:text-brand cursor-pointer'
+								>
 									Get started
 								</Button>
 							</Link>
 							<Link href={'/'}>
 								<Button
-									variant={'outline'}
-									className='text-primary-foreground font-inter text-lg px-[2rem] py-[1.5rem]'
+									variant={'secondary'}
+									className='text-foreground font-inter text-lg px-[2rem] py-[1.5rem] cursor-pointer'
 								>
 									See demo
 								</Button>
