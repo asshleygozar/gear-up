525f220595fdc79129f05d843f33799f96ec1ecb
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-18 19:25:32 +0800

    feat!: implement real time net balance data display - update net balance model function return data for optimization - add net balance card component to dashboard home page - implement api route for fetching net balance - implement client side api fetching using tanstack query for fetching net balance array data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,24 +9,13 @@ import {
 } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 import user from '@/utils/accounts';
+import NetBalance from '@/components/DashboardCards';
 
 function Home() {
 	return (
 		<div className='xl:h-screen lg:h-full w-full bg-background-color grid xl:grid-rows-[210px_minmax(0,_1fr)] lg:grid-rows-auto-fit gap-[1rem] p-[2rem]'>
 			<section className='grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-[1rem] h-full'>
-				<Card className='relative'>
-					<CardHeader>
-						<CardTitle>Current balance</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<p className='text-3xl'>
-							$ {user[0].accounts[0].cash.totalBalance}
-						</p>
-					</CardContent>
-					<CardFooter className='absolute bottom-5 right-1'>
-						{format(new Date(), 'MMMM dd, yyyy')}
-					</CardFooter>
-				</Card>
+				<NetBalance />
 				<Card className='relative'>
 					<CardHeader>
 						<CardTitle>Total income</CardTitle>
