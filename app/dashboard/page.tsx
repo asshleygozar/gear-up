f935f3b7601641839b78cb0dd9bda2528698d47d
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-19 11:55:23 +0800

    feat!: implement total income and total expense dashboard cards - add model function for querying accounts total incomes and total expense - implement server actions for serving client - implement dashboard cards for displaying total income and total expenses

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,37 +9,19 @@ import {
 } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 import user from '@/utils/accounts';
-import NetBalance from '@/components/DashboardCards';
+import {
+	NetBalance,
+	TotalExpense,
+	TotalIncome,
+} from '@/components/DashboardCards';
 
 function Home() {
 	return (
 		<div className='xl:h-screen lg:h-full w-full bg-background-color grid xl:grid-rows-[210px_minmax(0,_1fr)] lg:grid-rows-auto-fit gap-[1rem] p-[2rem]'>
 			<section className='grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-[1rem] h-full'>
 				<NetBalance />
-				<Card className='relative'>
-					<CardHeader>
-						<CardTitle>Total income</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<p className='text-3xl'>$ {user[0].accounts[0].cash.totalIncome}</p>
-					</CardContent>
-					<CardFooter className='absolute bottom-5 right-1'>
-						{format(new Date(), 'MMMM dd, yyyy')}
-					</CardFooter>
-				</Card>
-				<Card className='relative'>
-					<CardHeader>
-						<CardTitle>Total expenses</CardTitle>
-					</CardHeader>
-					<CardContent>
-						<p className='text-3xl'>
-							$ {user[0].accounts[0].cash.totalExpense}
-						</p>
-					</CardContent>
-					<CardFooter className='absolute bottom-5 right-1'>
-						{format(new Date(), 'MMMM dd, yyyy')}
-					</CardFooter>
-				</Card>
+				<TotalIncome />
+				<TotalExpense />
 			</section>
 			<section className='grid xl:grid-cols-2 lg:grid-cols-1 gap-[1rem]'>
 				<Card>
--- server/balance.ts
+++ server/balance.ts
@@ -1,6 +1,10 @@
 'use server';
 
-import { getNetBalance as queryNetBalance } from '@/model/accounts';
+import {
+	getNetBalance as queryNetBalance,
+	getAllIncomes as queryTotalIncome,
+	getAllExpenses as queryTotalExpense,
+} from '@/model/accounts';
 import { ServerResponse } from './response';
 
 export async function getNetBalance(): Promise<ServerResponse<number[]>> {
@@ -23,6 +27,66 @@ export async function getNetBalance(): Promise<ServerResponse<number[]>> {
 		return {
 			success: false,
 			message: 'An error occured while fetching net balance data',
+			error: `${error}`,
+		};
+	}
+}
+
+export async function getTotalIncomes(): Promise<ServerResponse<number[]>> {
+	try {
+		const netIncome = await queryTotalIncome();
+
+		if (
+			!netIncome.success ||
+			netIncome.data === null ||
+			netIncome.data === undefined
+		) {
+			return {
+				success: false,
+				message: 'No net income data found',
+			};
+		}
+
+		return {
+			success: true,
+			message: 'Successful!',
+			data: netIncome.data,
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			success: false,
+			message: ' An error occured whil fetching net income balance',
+			error: `${error}`,
+		};
+	}
+}
+
+export async function getTotalExpenses(): Promise<ServerResponse<number[]>> {
+	try {
+		const netIncome = await queryTotalExpense();
+
+		if (
+			!netIncome.success ||
+			netIncome.data === null ||
+			netIncome.data === undefined
+		) {
+			return {
+				success: false,
+				message: 'No net income data found',
+			};
+		}
+
+		return {
+			success: true,
+			message: 'Successful!',
+			data: netIncome.data,
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			success: false,
+			message: ' An error occured whil fetching net income balance',
 			error: `${error}`,
 		};
 	}
