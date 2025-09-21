aa10b6c56236ef83e39d36e166c44a7d49cc9617
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-21 23:10:20 +0800

    feat: implement model function for querying user all transactions

--- model/transaction.ts
+++ model/transaction.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from '@/lib/generated/prisma';
+import { PrismaClient, transaction_table } from '@/lib/generated/prisma';
 import { TransactionTypes } from '@/app/api/transaction/route';
 import { ModelResponse } from './response';
+import { getCurrentUserSession } from './user';
 
 const prisma = new PrismaClient();
 
@@ -43,6 +44,39 @@ export async function createTransaction({
 			success: false,
 			message: 'Failed to insert transaction',
 			error: 'An error occured while inserting your transation',
+		};
+	}
+}
+
+export async function getAllTransactions(): Promise<
+	ModelResponse<transaction_table[]>
+> {
+	try {
+		const userId = await getCurrentUserSession();
+		if (!userId.data || userId.data === null) {
+			return {
+				success: false,
+				message: 'User not found! Session expired',
+			};
+		}
+
+		const transactions = await prisma.transaction_table.findMany({
+			where: {
+				user_id: userId.data.user_id,
+			},
+		});
+
+		return {
+			success: true,
+			message: 'Query successful!',
+			data: transactions,
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			success: false,
+			message: 'Failed to query transactions',
+			error: ` An error occured: ${error}`,
 		};
 	}
 }
