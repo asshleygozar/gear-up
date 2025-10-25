e46cf762e31bbe40a137480cdf449a8fe3336452
Asshley <asshleydeanwaynegozar@gmail.com>
2025-10-25 12:23:47 +0800

    refactor(migrate)!: remove server side logic from nextjs for conciseness - remove models - remove server actions - remove api routes - remove jwt tokens sessions - remove prisma - remove dashboard cards - uninstall server dev dependencies and dependencies

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,44 +1,8 @@
-'use client';
-import { Card, CardHeader, CardTitle } from '@/components/ui/card';
-import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
-import user from '@/utils/accounts';
-import {
-	NetBalance,
-	TotalExpense,
-	TotalIncome,
-} from '@/components/DashboardCards';
-
 function Home() {
 	return (
 		<div className='xl:h-screen lg:h-full w-full bg-background-color grid xl:grid-rows-[210px_minmax(0,_1fr)] lg:grid-rows-auto-fit gap-[1rem] p-[2rem]'>
-			<section className='grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-[1rem] h-full'>
-				<NetBalance />
-				<TotalIncome />
-				<TotalExpense />
-			</section>
-			<section className='grid xl:grid-cols-2 lg:grid-cols-1 gap-[1rem]'>
-				<Card>
-					<CardHeader>
-						<CardTitle>Recent transactions</CardTitle>
-					</CardHeader>
-					<div className='p-[1rem]'>
-						<Table>
-							<TableBody>
-								{user[0].transactions.map((transaction) => (
-									<TableRow key={transaction.id}>
-										<TableCell className='font-quick-sand'>
-											{transaction.category}
-										</TableCell>
-										<TableCell className='text-brand'>
-											${transaction.amount}
-										</TableCell>
-									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-					</div>
-				</Card>
-			</section>
+			<section className='grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-[1rem] h-full'></section>
+			<section className='grid xl:grid-cols-2 lg:grid-cols-1 gap-[1rem]'></section>
 		</div>
 	);
 }
--- frontend/model/response.ts
+++
@@ -1,7 +0,0 @@
-export type ModelResponse<T = unknown> = {
-	success: boolean;
-	message: string;
-	error?: string;
-	errors?: Record<string, string[]>;
-	data?: T;
-};
