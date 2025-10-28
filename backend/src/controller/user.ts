941680a1ae12207aa5e8fa18b94415e7f85d9e5d
Asshley <asshleydeanwaynegozar@gmail.com>
2025-10-28 22:43:47 +0800

    feat: implement create user controller

--- backend/src/controller/user.ts
+++ backend/src/controller/user.ts
@@ -1,67 +1,109 @@
-import { getUserInfo, verifyPassword } from '../model/user.js';
-import { SignInSchema } from '../lib/auth.js';
-import { Request, Response } from 'express';
-import { createSession } from '../lib/session.js';
+import { createUser, getUserInfo, verifyPassword } from "../model/user.js";
+import { SignInSchema } from "../lib/auth.js";
+import { Request, Response } from "express";
+import { createSession } from "../lib/session.js";
+import { isUsernameExists as queryUsername } from "../model/user.js";
 
 export async function signIn(request: Request, response: Response) {
-	try {
-		if (!request.body) {
-			return response.status(400).json({
-				success: false,
-				message: 'No request body provided',
-				error: 'Invalid request',
-			});
-		}
+    try {
+        if (!request.body) {
+            return response.status(400).json({
+                success: false,
+                message: "No request body provided",
+                error: "Invalid request",
+            });
+        }
 
-		const { email, password } = request.body;
-		const data = {
-			email: email as string,
-			password: password as string,
-		};
+        const { email, password } = request.body;
+        const data = {
+            email: email as string,
+            password: password as string,
+        };
 
-		const validation = SignInSchema.safeParse(data);
+        const validation = SignInSchema.safeParse(data);
 
-		if (!validation.success) {
-			return response
-				.json({
-					success: false,
-					message: 'Validation failed',
-					error: 'Invalid format',
-					errors: validation.error.flatten().fieldErrors,
-				})
-				.end();
-		}
+        if (!validation.success) {
+            return response
+                .json({
+                    success: false,
+                    message: "Validation failed",
+                    error: "Invalid format",
+                    errors: validation.error.flatten().fieldErrors,
+                })
+                .end();
+        }
 
-		const user = await getUserInfo(data.email);
+        const user = await getUserInfo(data.email);
 
-		if (!user.success || !user.data)
-			return response.json({
-				success: false,
-				message: 'Failed to find user',
-				error: 'User not found',
-			});
+        if (!user.success || !user.data)
+            return response.json({
+                success: false,
+                message: "Failed to find user",
+                error: "User not found",
+            });
 
-		const isPasswordValid = await verifyPassword({
-			password: data.password,
-			recordedPassword: user.data.password ?? '',
-		});
+        const isPasswordValid = await verifyPassword({
+            password: data.password,
+            recordedPassword: user.data.password ?? "",
+        });
 
-		if (!isPasswordValid.success)
-			return response.json({
-				success: false,
-				message: 'Failed to validate password',
-				error: 'Invalid password',
-			});
+        if (!isPasswordValid.success)
+            return response.json({
+                success: false,
+                message: "Failed to validate password",
+                error: "Invalid password",
+            });
 
-		await createSession({ userId: user.data.user_id, response: response });
+        await createSession({ userId: user.data.user_id, response: response });
 
-		return response.json({ success: true, message: 'Signed in successfully!' });
-	} catch (error) {
-		console.error('Server error: ', error);
-		return response.status(500).json({
-			success: false,
-			message: 'Authentication failed',
-			error: 'Server error',
-		});
-	}
+        return response.json({ success: true, message: "Signed in successfully!" });
+    } catch (error) {
+        console.error("Server error: ", error);
+        return response.status(500).json({
+            success: false,
+            message: "Authentication failed",
+            error: "Server error",
+        });
+    }
+}
+
+export async function signUp(request: Request, response: Response) {
+    try {
+        if (!request.body) {
+            return response.status(400).json({
+                success: false,
+                message: "Failed to receive form data",
+                error: "No body provided",
+            });
+        }
+
+        const { email, password, first_name, last_name, username } = request.body;
+
+        const isUsernameExists = await queryUsername(username);
+
+        if (!isUsernameExists.success) {
+            return response.json({
+                success: false,
+                message: "Username taken please user another username",
+                error: "Username taken",
+            });
+        }
+
+        const user = await createUser({ ...request.body });
+
+        if (!user.success || !user.data) {
+            return response.json({ success: false, message: "Failed to create user", error: user.error });
+        }
+
+        await createSession({ userId: user.data.user_id, response: response });
+
+        return response.status(200).json({ success: true, message: "Account created successfully!" });
+    } catch (error) {
+        console.error("Server error: ", error);
+        return response.json({
+            success: false,
+            message: "Failed to create account",
+            error: "Server error",
+        });
+    }
 }
