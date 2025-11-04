5471c606e823d7cfe3abae2a81c7205a17a86669
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-04 23:08:54 +0800

    fix: update proccess env environment to node env for standard approach

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -4,4 +4,4 @@ const globalPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalPrisma.prisma || new PrismaClient();
 
-if (process.env.APP_STAGE !== "production") globalPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
