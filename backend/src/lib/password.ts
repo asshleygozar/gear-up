b842b07e909c132d6b3a10b18bb7ff92868bef81
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-04 23:13:49 +0800

    feat(lib): implement password lib for encrypting and comparing password - update hash password function name to present tense for clarity - add compare password function for validating password - add env salt attribute for implementing environment based rounded salt

---
+++ backend/src/lib/password.ts
@@ -0,0 +1,10 @@
+import { compare, hash } from "bcryptjs";
+import { env } from "env.ts";
+
+export const hashPassword = async (password: string) => {
+    return await hash(password, env.SALT);
+};
+
+export const comparePassword = async (password: string, recordedPassword: string): Promise<boolean> => {
+    return await compare(password, recordedPassword);
+};
