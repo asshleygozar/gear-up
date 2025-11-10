1fdfe7963389c923ef2fd27c3e10248b19309a54
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-10 22:35:50 +0800

    update port log name for clarity

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import app from "#app.ts";
 import { env } from "env.ts";
 
 app.listen(env.PORT, () => {
-    console.log(`Server running on PORT http://localhost:${env.PORT}`);
+    console.log(`Server running on PORT ${env.PORT}`);
     console.log(`Environment: ${env.APP_STAGE}`);
 });
