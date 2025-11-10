4f513a78fb84e712b50aa4b671b6474a3a5c4121
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-10 13:30:42 +0800

    chore: add dynamic database url for dynamic environment setup

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig, env } from 'prisma/config';
-import 'dotenv/config';
-import { config } from 'dotenv';
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
+import { config } from "dotenv";
+import { env } from "./env";
 
 config();
 
 export default defineConfig({
-	schema: 'prisma/schema.prisma',
-	migrations: {
-		path: 'prisma/migrations',
-	},
-	engine: 'classic',
-	datasource: {
-		url: env('DATABASE_URL'),
-	},
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    engine: "classic",
+    datasource: {
+        url: env.DATABASE_URL,
+    },
 });
