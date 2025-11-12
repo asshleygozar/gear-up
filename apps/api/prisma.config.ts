85e2937d4a9de149658adb0ad649761d6213b1ab
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-12 23:17:42 +0800

    build(monorepo)!: introduce monorepo for managing web and api - implement centralized json for managing both web and api applications - update api tsconfig for consistency - remove gitignore for both web and api - update api prisma config typescript env path file

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "prisma/config";
 import "dotenv/config";
 import { config } from "dotenv";
-import { env } from "env.ts";
+import { env } from "./env.ts";
 
 config();
 
