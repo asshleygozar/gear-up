24e8955fdedb838b66e7431f00609809513f626b
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-15 16:20:53 +0800

    fix: remove excess dot for consistency with file naming convention

--- apps/api/src/routes/auth.route.ts
+++ apps/api/src/routes/auth.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { signIn, signUp } from "../controllers/auth.controller.ts";
-import { validateBody } from "../middlewares/validation.middleware.ts";
-import { SignInSchema, SignUpSchema } from "../lib/auth.ts";
+import { signIn, signUp } from "#controllers/auth.controller.js";
+import { validateBody } from "#middlewares/validation.middleware.js";
+import { SignInSchema, SignUpSchema } from "#lib/auth.js";
 
 const router = Router();
 
