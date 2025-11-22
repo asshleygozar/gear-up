82f99089a048ca99db55700f829ad8d52fbc1f04
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-22 21:13:49 +0800

    feat: implement proxy api for interacting with the server

---
+++ apps/web/app/api/auth/signup/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+	try {
+		const body = await request.json();
+
+		const apiResponse = await fetch(
+			`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/signup`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			}
+		);
+
+		const data = await apiResponse.json();
+
+		if (!apiResponse.ok)
+			return NextResponse.json(data, { status: apiResponse.status });
+
+		return NextResponse.json(data).cookies.set('token', data.token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'lax',
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+			path: '/',
+		});
+	} catch (error) {
+		console.error('Server error: ', error);
+	}
+}
