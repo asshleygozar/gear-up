d5dbf61f6bf29d5c5f6c86f14a0f0b0e7e0e116b
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-22 23:52:01 +0800

    fix: update request path url checking for organization and consistency

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,41 +3,60 @@ import { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
 	try {
-		const { pathname } = request.nextUrl;
 		const token = request.cookies.get('token')?.value;
 		const protectedRoutes = ['/dashboard'];
-		const publicRoutes = ['/', '/signin', '/signup'];
-		const isProtectedRoutes = protectedRoutes.some((route) =>
+		const publicRoutes = ['/'];
+		const authRoutes = ['/signin', '/signup'];
+
+		const isProtectedRoute = protectedRoutes.some((route) =>
 			request.nextUrl.pathname.startsWith(route)
 		);
-		const isPublic = publicRoutes.includes(pathname);
-
-		if (!token) {
-			if (isProtectedRoutes) {
-				return NextResponse.redirect(new URL('/signin', request.url));
-			}
-			return NextResponse.next();
-		}
-		const response = await fetch(
-			`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/validate`,
-			{
-				method: 'POST',
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-				credentials: 'include',
-			}
+		const isPublicRoutes = publicRoutes.some((route) =>
+			request.nextUrl.pathname.startsWith(route)
+		);
+		const isAuthRoute = authRoutes.some((route) =>
+			request.nextUrl.pathname.startsWith(route)
 		);
 
-		if (!response.ok) {
-			if (isProtectedRoutes) {
-				NextResponse.redirect(new URL('/signin', request.url));
-			}
+		// Checks if has token + on landing page or auth page then if token is valid, redirects to dashboard
+		if (token && (isPublicRoutes || isAuthRoute)) {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/validate`,
+				{
+					method: 'POST',
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+					credentials: 'include',
+				}
+			);
 
-			return NextResponse.next();
+			if (response.ok) {
+				return NextResponse.redirect(new URL('/dashboard', request.url));
+			}
 		}
-		if (response.ok && isPublic) {
-			return NextResponse.redirect(new URL('/dashboard', request.url));
+
+		// Checks is no token and accessing protected routes
+		if (!token && isProtectedRoute) {
+			return NextResponse.redirect(new URL('/signin', request.url));
+		}
+
+		// Checks if has token and accessing protected routes
+		if (token && isProtectedRoute) {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/validate`,
+				{
+					method: 'POST',
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+					credentials: 'include',
+				}
+			);
+
+			if (!response.ok) {
+				return NextResponse.redirect(new URL('/signin', request.url));
+			}
 		}
 
 		return NextResponse.next();
