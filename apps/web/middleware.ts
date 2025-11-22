cfb8182da1d8f169062142083c64d1ee24b11e41
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-22 12:10:49 +0800

    fix: add missing cookie value

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
 	try {
-		const token = request.cookies.get('token');
+		const token = request.cookies.get('token')?.value;
 		const protectedRoutes = ['/dashboard'];
 		const isProtectedRoutes = protectedRoutes.some((route) =>
 			request.nextUrl.pathname.startsWith(route)
@@ -21,8 +21,10 @@ export async function middleware(request: NextRequest) {
 			}
 		);
 
-		if (response.status === 401)
+		if (response.status === 401) {
 			return NextResponse.redirect(new URL('/signin', request.url));
+		}
+
 		return NextResponse.next();
 	} catch (error) {
 		return NextResponse.redirect(new URL('/signin', request.url));
