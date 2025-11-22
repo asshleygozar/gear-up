580e27a20ae3f3bdd3ccec4d406b9338e5dc8cac
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-22 11:53:43 +0800

    fix: update cookie header to allow manual reading on edge runtime

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,24 +3,27 @@ import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
 	try {
+		const token = request.cookies.get('token');
 		const protectedRoutes = ['/dashboard'];
-
 		const isProtectedRoutes = protectedRoutes.some((route) =>
 			request.nextUrl.pathname.startsWith(route)
 		);
 		if (!isProtectedRoutes) return NextResponse.next();
-
+		if (!token) return NextResponse.redirect(new URL('/signin', request.url));
 		const response = await fetch(
 			`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/validate`,
 			{
 				method: 'POST',
+				headers: {
+					Cookie: `token=${token}`,
+				},
 				credentials: 'include',
 			}
 		);
 
 		if (response.status === 401)
 			return NextResponse.redirect(new URL('/signin', request.url));
-		NextResponse.next();
+		return NextResponse.next();
 	} catch (error) {
 		return NextResponse.redirect(new URL('/signin', request.url));
 	}
