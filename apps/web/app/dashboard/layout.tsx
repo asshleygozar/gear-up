c8cc8b52770d7e42e158f7dd69e55ce349a4adc4
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-23 14:06:37 +0800

    feat!: implement token validation before dashboard renders for ux and security

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,8 +1,40 @@
+'use client';
+
 import { LeftSidebar } from '@/components/LeftSidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import QueryProvider from './provider';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+	const router = useRouter();
+	useEffect(() => {
+		const session = async () => {
+			const token = await cookieStore.get('token');
+
+			if (!token?.value) {
+				router.push('/signin');
+			}
+
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/validate`,
+				{
+					method: 'POST',
+					headers: {
+						Authorization: `Bearer ${token?.value}`,
+					},
+					credentials: 'include',
+				}
+			);
+
+			if (!response.ok) {
+				await cookieStore.delete('token');
+				router.push('/signin');
+			}
+		};
+
+		session();
+	});
 	return (
 		<QueryProvider>
 			<SidebarProvider>
