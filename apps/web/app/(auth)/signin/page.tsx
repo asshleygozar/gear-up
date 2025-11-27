05f19eaf4162af9a7c6ff7028cba242d4fe7bf79
Asshley <asshleydeanwaynegozar@gmail.com>
2025-11-27 15:03:05 +0800

    feat: update sign in ui layout - move sign in with email page to dialog for simplicity - add brand logo design to sign in page - add custom google sign in button

--- apps/web/app/(auth)/signin/page.tsx
+++ apps/web/app/(auth)/signin/page.tsx
@@ -1,149 +1,47 @@
 'use client';
 
-import { useActionState } from 'react';
-import { useRouter } from 'next/navigation';
-import {
-	Form,
-	FormError,
-	FormGroup,
-	FormInput,
-	FormLabel,
-} from '@/components/ui/Form';
-import { Loader2Icon } from 'lucide-react';
-import Link from 'next/link';
+import { SignIn } from '@/components/Auth';
 import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import { APIResponse } from '@/utils/type';
-
-const initialState: APIResponse = {
-	success: false,
-	message: '',
-	error: '',
-	errors: [
-		{
-			field: '',
-			message: '',
-		},
-	],
-};
+import { handleGoogleSignIn } from '@/lib/auth';
+import Image from 'next/image';
 
 export default function SignInPage() {
-	const router = useRouter();
-
-	const handleSubmit = async (prevState: APIResponse, formData: FormData) => {
-		try {
-			const data = {
-				email: formData.get('email'),
-				password: formData.get('password'),
-			};
-			const result = await fetch(
-				`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/signin/`,
-				{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					credentials: 'include',
-					body: JSON.stringify(data),
-				}
-			);
-
-			const response = await result.json();
-
-			if (response.success) {
-				// Maybe add toast here like success
-
-				router.push('/dashboard');
-			}
-
-			return response;
-		} catch (error) {
-			console.error(error);
-			return {
-				success: false,
-				message:
-					'Failed to signed in, An error occured while authenticating your account',
-				error:
-					'Failed to signed in, An error occured while authenticating your account',
-			};
-		}
-	};
-
-	const [state, formAction, isPending] = useActionState<APIResponse, FormData>(
-		handleSubmit,
-		initialState
-	);
-
 	return (
-		<div className='h-screen flex justify-center items-center'>
-			<Form
-				action={formAction}
-				className='bg-card rounded-xl p-8 w-md h-fit flex flex-col gap-4 shadow-md'
-			>
-				<h1 className='text-center text-2xl font-inter font-semibold'>
-					Log into your account
-				</h1>
-				<FormError className='text-red-500'>{state.message}</FormError>
-				<FormGroup className='flex flex-col gap-2'>
-					{state?.errors && state?.errors && state.errors?.length > 0 && (
-						<FormError className='text-red-500 text-[0.9rem]'>
-							{state.errors.find((field) => field.field === 'email')?.message}
-						</FormError>
-					)}
-					<FormLabel htmlFor='email'>Email</FormLabel>
-					<FormInput
-						id='email'
-						name='email'
-						type='email'
-						disabled={isPending}
-						required
-						placeholder='Enter email'
-						className={cn(
-							'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-							'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-							'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
-						)}
-					/>
-				</FormGroup>
-				<FormGroup className='flex flex-col gap-2'>
-					{state?.errors && state?.errors && state.errors?.length > 0 && (
-						<FormError className='text-red-500 text-[0.9rem]'>
-							{
-								state.errors.find((field) => field.field === 'password')
-									?.message
-							}
-						</FormError>
-					)}
-					<FormLabel htmlFor='password'>Password</FormLabel>
-					<FormInput
-						id='password'
-						name='password'
-						type='password'
-						required
-						disabled={isPending}
-						placeholder='Enter password'
-						className={cn(
-							'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-							'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-							'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
-						)}
-					/>
-				</FormGroup>
+		<main className='h-screen grid grid-cols-2 items-center justify-items-center'>
+			<div className='grid gap-5'>
+				<header className='grid gap-3'>
+					<h1 className='text-center text-5xl font-inter font-extrabold'>
+						Welcome Back!
+					</h1>
+					<h2 className='text-center text-[1rem] text-muted-foreground font-quick-sand font-medium'>
+						Use other services or use your email to continue with GearUp.
+					</h2>
+				</header>
 				<Button
-					type='submit'
-					disabled={isPending}
-					className='bg-primary text-white text-md font-inter font-medium hover:bg-violet-500 cursor-pointer'
+					variant={'secondary'}
+					onClick={handleGoogleSignIn}
+					className='px-8 py-6 flex gap-8 text-foreground text-[1rem] font-quick-sand font-semibold '
 				>
-					{isPending ? <Loader2Icon className='animate-spin' /> : 'Sign in'}
+					<Image
+						src={'/google-icon.svg'}
+						alt='Google logo'
+						height={30}
+						width={30}
+					/>
+					Continue with Google
 				</Button>
-				<p className='text-center text-sm'>
-					Don&apos;t have an account?{' '}
-					<Link href={'/signup'}>
-						<span className='text-primary'>Sign up</span>
-					</Link>{' '}
-					instead
-				</p>
-			</Form>
-		</div>
+				<p className='text-center'>or</p>
+				<SignIn />
+			</div>
+			<div className='bg-primary h-screen w-[50vw] grid'>
+				<Image
+					src={'/gearup-logo-light.svg'}
+					alt=''
+					height={450}
+					width={450}
+					className='place-self-center'
+				/>
+			</div>
+		</main>
 	);
 }
---
+++ apps/web/components/Auth.tsx
@@ -0,0 +1,160 @@
+'use client';
+import {
+	Dialog,
+	DialogHeader,
+	DialogTitle,
+	DialogContent,
+	DialogTrigger,
+	DialogDescription,
+} from './ui/dialog';
+import { Form, FormError, FormGroup, FormLabel } from '@/components/ui/Form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Loader2Icon } from 'lucide-react';
+import { useActionState } from 'react';
+import { APIResponse } from '@/utils/type';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+const initialState: APIResponse = {
+	success: false,
+	message: '',
+	error: '',
+	errors: [
+		{
+			field: '',
+			message: '',
+		},
+	],
+};
+export const SignIn = () => {
+	const router = useRouter();
+
+	const handleSubmit = async (prevState: APIResponse, formData: FormData) => {
+		try {
+			const data = {
+				email: formData.get('email'),
+				password: formData.get('password'),
+			};
+			const result = await fetch(
+				`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/signin/`,
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					credentials: 'include',
+					body: JSON.stringify(data),
+				}
+			);
+
+			const response = await result.json();
+
+			if (response.success) {
+				router.push('/dashboard');
+			}
+
+			return response;
+		} catch (error) {
+			console.error(error);
+			return {
+				success: false,
+				message:
+					'Failed to signed in, An error occured while authenticating your account',
+				error:
+					'Failed to signed in, An error occured while authenticating your account',
+			};
+		}
+	};
+
+	const [state, formAction, isPending] = useActionState<APIResponse, FormData>(
+		handleSubmit,
+		initialState
+	);
+	return (
+		<Dialog>
+			<DialogTrigger className='px-8 py-3 bg-primary text-primary-foreground text-[1rem] text-center font-quick-sand font-semibold  border rounded-md'>
+				Continue with email
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle className='text-center text-3xl font-inter font-bold'>
+						Sign in
+					</DialogTitle>
+					<DialogDescription className='text-center text-[1rem] text-muted-foreground font-quick-sand font-medium'>
+						Sign in with your email here
+					</DialogDescription>
+				</DialogHeader>
+				<Form
+					action={formAction}
+					className='w-md h-fit flex flex-col gap-4'
+				>
+					<FormError className='text-red-500'>{state.message}</FormError>
+					<FormGroup className='flex flex-col gap-2'>
+						{state?.errors && state?.errors && state.errors?.length > 0 && (
+							<FormError className='text-red-500 text-[0.9rem]'>
+								{state.errors.find((field) => field.field === 'email')?.message}
+							</FormError>
+						)}
+						<FormLabel
+							htmlFor='email'
+							className='text-[0.9rem]'
+						>
+							Email
+						</FormLabel>
+						<Input
+							id='email'
+							name='email'
+							type='email'
+							disabled={isPending}
+							required
+							placeholder='Enter email'
+							className='font-quick-sand font-medium text-[1.5rem] py-5'
+						/>
+					</FormGroup>
+					<FormGroup className='flex flex-col gap-2'>
+						{state?.errors && state?.errors && state.errors?.length > 0 && (
+							<FormError className='text-red-500 text-[0.9rem]'>
+								{
+									state.errors.find((field) => field.field === 'password')
+										?.message
+								}
+							</FormError>
+						)}
+						<FormLabel
+							htmlFor='password'
+							className='text-[0.9rem]'
+						>
+							Password
+						</FormLabel>
+						<Input
+							id='password'
+							name='password'
+							type='password'
+							required
+							disabled={isPending}
+							placeholder='Enter password'
+							className='font-quick-sand font-medium text-[1.5rem] py-5'
+						/>
+					</FormGroup>
+					<Button
+						variant={'default'}
+						type='submit'
+						disabled={isPending}
+						className='px-8 py-6 flex gap-8 text-background text-[1rem] font-quick-sand font-semibold '
+					>
+						{isPending ? <Loader2Icon className='animate-spin' /> : 'Log in'}
+					</Button>
+
+					<p className='text-center text-sm font-quick-sand font-medium'>
+						Don&apos;t have an account?{' '}
+						<Link href={'/signup'}>
+							<span className='text-primary'>Sign up</span>
+						</Link>{' '}
+						instead
+					</p>
+				</Form>
+			</DialogContent>
+		</Dialog>
+	);
+};
