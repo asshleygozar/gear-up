d30f27e87faae78a01bb030c37e39fbc8b823e79
Asshley <asshleydeanwaynegozar@gmail.com>
2025-12-06 14:25:56 +0800

    feat(auth): implement proxy for authentication for cookie setting

---
+++ apps/web/app/api/auth/signin/route.ts
@@ -0,0 +1,41 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+export const POST = async (request: NextRequest) => {
+	try {
+		const body = await request.json();
+
+		const apiResponse = await fetch(
+			`${process.env.NEXT_PUBLIC_API_ORIGIN}/auth/signin`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			}
+		);
+
+		const data = await apiResponse.json();
+
+		if (!apiResponse.ok || !data.success) {
+			return NextResponse.json(data, { status: apiResponse.status });
+		}
+
+		const response = NextResponse.json(data);
+		response.cookies.set('token', data.token, {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'lax',
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+			path: '/',
+		});
+		return response;
+	} catch (error) {
+		if (error) {
+			return NextResponse.json({
+				success: false,
+				message: 'An error occured',
+			});
+		}
+	}
+};
