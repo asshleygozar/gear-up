3e5918597c89e387b51d277d7baafdc404714d14
Asshley <asshleydeanwaynegozar@gmail.com>
2025-12-16 21:37:46 +0800

    fix: update parameter order for clarity

--- apps/api/src/errors/general-error.ts
+++ apps/api/src/errors/general-error.ts
@@ -2,7 +2,7 @@ class GeneralError extends Error {
     message: string;
     status: number;
     name: string;
-    constructor(message: string, status: number, name: string) {
+    constructor(name: string, message: string, status: number) {
         super(message);
         this.message = message;
         this.status = status;
