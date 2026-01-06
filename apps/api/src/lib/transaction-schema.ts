ebfc3e4cec29a2b36b83a34b5192bbb47f1fa331
Asshley <asshleydeanwaynegozar@gmail.com>
2026-01-06 22:19:21 +0800

    fix!: revert transaction type to create transaction revert transaction type because update transaction should include transaction id but transaction type does not have any. I will create new schema specifically for update transaction for consistency

--- apps/api/src/lib/transaction-schema.ts
+++ apps/api/src/lib/transaction-schema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-export const TransactionSchema = z.object({
+export const CreateTransactionSchema = z.object({
     transaction_category: z.string(),
     transaction_amount: z.coerce.number(),
     transaction_date: z.string().datetime(),
@@ -10,4 +10,4 @@ export const TransactionSchema = z.object({
     account_id: z.coerce.number(),
 });
 
-export type TransactionType = z.infer<typeof TransactionSchema>;
+export type CreateTransactionType = z.infer<typeof CreateTransactionSchema>;
