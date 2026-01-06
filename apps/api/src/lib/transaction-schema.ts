4d30fd7980fcdf498fe881ea86fc4d1c0b021dc3
Asshley <asshleydeanwaynegozar@gmail.com>
2026-01-06 21:14:14 +0800

    fix: update transaction schema name for reusability I update the transaction zod schema name to TransactionSchema because CreateTransactionSchema has the same structure with UpdateTransactionSchema so it can be used again without conflicting with the schema name.

--- apps/api/src/lib/transaction-schema.ts
+++ apps/api/src/lib/transaction-schema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-export const CreateTransactionSchema = z.object({
+export const TransactionSchema = z.object({
     transaction_category: z.string(),
     transaction_amount: z.coerce.number(),
     transaction_date: z.string().datetime(),
@@ -10,4 +10,4 @@ export const CreateTransactionSchema = z.object({
     account_id: z.coerce.number(),
 });
 
-export type CreateTransactionType = z.infer<typeof CreateTransactionSchema>;
+export type TransactionType = z.infer<typeof TransactionSchema>;
