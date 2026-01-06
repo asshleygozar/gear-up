a3dae59c944def1f0d98f6843eb9d98177dd64f2
Asshley <asshleydeanwaynegozar@gmail.com>
2026-01-06 21:23:00 +0800

    fix: update transaction schema import

--- apps/api/src/routes/transaction.route.ts
+++ apps/api/src/routes/transaction.route.ts
@@ -1,11 +1,11 @@
 // Transactions Route
 import { Router } from "express";
-import { CreateTransactionSchema } from "#lib/transaction-schema.js";
+import { TransactionSchema } from "#lib/transaction-schema.js";
 import { validateBody } from "#middlewares/validation.middleware.js";
 import { createTransaction, getAllTransactions } from "#controllers/transaction.controller.js";
 
 const router = Router();
 router.get("/", getAllTransactions);
-router.post("/create", validateBody(CreateTransactionSchema), createTransaction);
+router.post("/create", validateBody(TransactionSchema), createTransaction);
 
 export default router;
